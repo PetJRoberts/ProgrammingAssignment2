45a6247f9339765ebc7578efd8cec2061aa4387f
PetJRoberts <peterjroberts83@hotmail.com>
2016-03-23 17:45:23 +1100

    Added files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## These functions follow similarly the framework described in the
+## Caching the Mean of the Vector example
 
 ## Write a short comment describing this function
 
@@ -8,7 +10,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function checks to see if there exists a cached version of the inverse
+## of the matrix x
+## If there is the inverse is returned
+## If not the inverse of the matrix is found and returned
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
